7ce9b48849cd36b59f164252d961825424bc4592
Kevin Eslava <kevinalexisesma@ufps.edu.co>
2024-12-26 11:01:04 -0500

    Feat: Agregando componente service

--- src/main/java/com/kevin/springboot/app/aop/springboot_aop/controllers/GreetingController.java
+++ src/main/java/com/kevin/springboot/app/aop/springboot_aop/controllers/GreetingController.java
@@ -2,15 +2,22 @@ package com.kevin.springboot.app.aop.springboot_aop.controllers;
 
 import java.util.Collections;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.kevin.springboot.app.aop.springboot_aop.services.GreetingService;
+
 @RestController
 public class GreetingController {
 
+    @Autowired
+    private GreetingService greetingService;
+
     @GetMapping("/greeting")
     public ResponseEntity<?> greeting(){
-        return ResponseEntity.ok(Collections.singletonMap("greeting", null));
+        return ResponseEntity.ok(Collections.singletonMap("greeting", 
+        greetingService.sayHello("Pepe", "Hola que tal")));
     }
 }
---
+++ src/main/java/com/kevin/springboot/app/aop/springboot_aop/services/GreetingService.java
@@ -0,0 +1,7 @@
+package com.kevin.springboot.app.aop.springboot_aop.services;
+
+
+public interface GreetingService {
+
+    String sayHello(String person, String phrase);
+}
