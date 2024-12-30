f6dab9dabd2e6d06bb72ccd999237627c0671f74
Kevin Eslava <kevinalexisesma@ufps.edu.co>
2024-12-30 11:13:57 -0500

    Feat: AfterReturning y AfterThrowing

--- src/main/java/com/kevin/springboot/app/aop/springboot_aop/GreetingAspect.java
+++ src/main/java/com/kevin/springboot/app/aop/springboot_aop/GreetingAspect.java
@@ -4,6 +4,8 @@ import java.util.Arrays;
 
 import org.aspectj.lang.JoinPoint;
 import org.aspectj.lang.annotation.After;
+import org.aspectj.lang.annotation.AfterReturning;
+import org.aspectj.lang.annotation.AfterThrowing;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
 import org.slf4j.Logger;
@@ -28,5 +30,19 @@ public class GreetingAspect {
         String method = joinPoint.getSignature().getName();
         String args = Arrays.toString(joinPoint.getArgs());
         logger.info("Despues: " + method + " con los argumentos " + args);
+    }
+
+    @AfterReturning("execution(* com.kevin.springboot.app.aop.springboot_aop.services.GreetingService.*(..))")
+    public void LoggerAfterReturning(JoinPoint joinPoint) {
+        String method = joinPoint.getSignature().getName();
+        String args = Arrays.toString(joinPoint.getArgs());
+        logger.info("Despues de retornar: " + method + " con los argumentos " + args);
+    }
+
+    @AfterThrowing("execution(* com.kevin.springboot.app.aop.springboot_aop.services.GreetingService.*(..))")
+    public void LoggerAfterThrowing(JoinPoint joinPoint) {
+        String method = joinPoint.getSignature().getName();
+        String args = Arrays.toString(joinPoint.getArgs());
+        logger.info("Despues de lanzar la excepcion: " + method + " con los argumentos " + args);
     }
 }
