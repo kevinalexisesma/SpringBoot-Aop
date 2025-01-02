51a24557ebf5dd23a16f354aa39025da291d756e
kevinalexisesma <81497048+kevinalexisesma@users.noreply.github.com>
2025-01-02 19:07:12 +0000

    Feat: Ordenando los aspectos con Order

---
+++ src/main/java/com/kevin/springboot/app/aop/springboot_aop/GreetingFooAspect.java
@@ -0,0 +1,34 @@
+package com.kevin.springboot.app.aop.springboot_aop;
+
+import java.util.Arrays;
+
+import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.annotation.After;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.core.annotation.Order;
+import org.springframework.stereotype.Component;
+
+@Order(1)
+@Aspect
+@Component
+public class GreetingFooAspect {
+
+    private Logger logger = LoggerFactory.getLogger(getClass());
+
+    @Before("execution(* com.kevin.springboot.app.aop.springboot_aop.services.GreetingService.*(..))")
+    public void LoggerBefore(JoinPoint joinPoint) {
+        String method = joinPoint.getSignature().getName();
+        String args = Arrays.toString(joinPoint.getArgs());
+        logger.info("Antes Primero: " + method + " invocado con los parámetros " + args);
+    }
+
+    @After("execution(* com.kevin.springboot.app.aop.springboot_aop.services.GreetingService.*(..))")
+    public void LoggerAfter(JoinPoint joinPoint) {
+        String method = joinPoint.getSignature().getName();
+        String args = Arrays.toString(joinPoint.getArgs());
+        logger.info("Despues Primero: " + method + " con los argumentos " + args);
+    }
+}
